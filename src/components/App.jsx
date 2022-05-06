2185603ec7e80ffd61d6f400ae2a7c5cb0a18e41
Ricardo Delgado <ricardodelgado@Ricardos-Mini.lan>
2022-05-06 17:28:53 -0500

    Trying Div100vh with another component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Main from "./Main";
-import Div100vh from "react-div-100vh";
 
 const App = () => {
   return (
-    <Div100vh>
-      <div className="App">
-        <Main />
-      </div>
-    </Div100vh>
+    <div className="App">
+      <Main />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
+import Div100vh from "react-div-100vh";
 
 ReactDOM.render(
-  <App />,
+  <Div100vh>
+    <App />
+  </Div100vh>,
   document.getElementById('root')
 );
