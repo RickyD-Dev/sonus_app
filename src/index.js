8e5b836cc41aee3d65f196b4d26d9920d5ce925e
Ricardo Delgado <ricardodelgado@Ricardos-Mini.lan>
2022-05-06 17:40:43 -0500

    Adding padding-bottom: env(safe-area-inset-bottom) to footer to see if this will help the safari ios layout issue.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
-import Div100vh from "react-div-100vh";
 
 ReactDOM.render(
-  <Div100vh>
-    <App />
-  </Div100vh>,
+  <App />,
   document.getElementById('root')
 );
