b47c922867962d6664f9de7dc2cf49af978698bb
Ricardo Delgado <ricardodelgado@Ricardos-Mini.lan>
2022-05-10 16:12:19 -0500

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Main from "./Main";
+import { Howler } from "howler";
 
 const App = () => {
+  function checkHowlerCtx() {
+    console.log("Touch Start began.");
+    console.log("This is the Howler ctx state over HERE: " + Howler.ctx.state);
+    if (Howler.ctx.state === "suspended") {
+        Howler.ctx.resume().then(() => {
+            console.log("Howler ctx SHOULD be resumed: " + Howler.ctx.state);
+        })
+    }
+  }
   return (
     <div className="App">
-      <Main />
+      <Main 
+        onTouchStart={checkHowlerCtx}
+      />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -178,15 +178,6 @@ function Main() {
         });
     }
 
-    useEffect(() => {
-        console.log("This is the Howler ctx state over HERE: " + Howler.ctx.state);
-        if (Howler.ctx.state === "suspended") {
-            Howler.ctx.resume().then(() => {
-                console.log("Howler ctx SHOULD be resumed: " + Howler.ctx.state);
-            })
-        }
-    }, [Howler.ctx.state]);
-
     // When true, the sounds are allowed to play. When false, the sound stops.
     const stringOne = useRef(true);
     const stringTwo = useRef(true);
